a968b1bc787174208a14acc28ac1dd8a57def8e1
Roman <noteR@MacBook-Pro-Roman.local>
2018-02-08 01:41:45 +0200

    Parsing json data for all cities at once, and storing them into weatherList array for further usage

--- Controller/MainVC.swift
+++ Controller/MainVC.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+//Struct for hardcoded array
 struct City {
    let id:String
    let name:String
@@ -16,12 +17,27 @@ struct City {
 //API key for OpenWeatherMap
 let apikey = "d2bd923726d8850b7677856f80cb52cd"
 
+//Array for weather Data for all hardcoded cities
+var weatherList = [List]()
+
+//Array for getting cities id's
+var iDsArr = [String]()
+
+//Hardcoded array of cities
+ let hardcodedCities = [City(id:"702550", name:"Lviv"), City(id:"2988507", name:"Paris"), City(id:"3128760", name:"Barcelona"), City(id:"292223", name:"Dubai"), City(id:"184745", name:"Nairobi"), City(id:"993800", name:"Johannesburg"), City(id:"1642911", name:"Jakarta"), City(id:"1850144", name:"Tokyo"), City(id:"1796236", name:"Shanghai"), City(id:"1273294", name:"Delhi"), City(id:"2147714", name:"Sydney"), City(id:"3936456", name:"Lima"), City(id:"3435910", name:"Buenos Aires"), City(id:"5856195", name:"Honolulu"), City(id:"5368361", name:"Los Angeles"), City(id:"5128581", name:"New York"), City(id:"6167865", name:"Toronto"), City(id:"5879400", name:"Anchorage"), ]
+
+
+
 class MainVC: UITableViewController {
-    
-    let hardcodedCities = [City(id:"702550", name:"Lviv"), City(id:"2988507", name:"Paris"), City(id:"3128760", name:"Barcelona"), City(id:"292223", name:"Dubai"), City(id:"184745", name:"Nairobi"), City(id:"993800", name:"Johannesburg"), City(id:"1642911", name:"Jakarta"), City(id:"1850144", name:"Tokyo"), City(id:"796236", name:"Shanghai"), City(id:"1273294", name:"Delhi"), City(id:"2147714", name:"Sydney"), City(id:"3936456", name:"Lima"), City(id:"3435910", name:"Buenos Aires"), City(id:"856195", name:"Honolulu"), City(id:"5368361", name:"Los Angeles"), City(id:"5128581", name:"New York"), City(id:"6167865", name:"Toronto"), City(id:"5879400", name:"Anchorage"), ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //function for getting cities id's
+        getIDsArr()
+        
+        //parsing json data for all cities at once, and storing them into weatherList array for further usage
+        parseWeatherJson()
         
     }
 
@@ -29,6 +45,7 @@ class MainVC: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         return hardcodedCities.count
+        
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -48,6 +65,60 @@ class MainVC: UITableViewController {
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 50
     }
+    
 
 }//class
+
+func getIDsArr() {
+
+        for var i in 0..<hardcodedCities.count {
+            iDsArr.append(hardcodedCities[i].id)
+            i+=1
+    }
+}//func
+
+func parseWeatherJson() {
+    
+    //transform array of strings into string
+    let iDsList = iDsArr.joined(separator: ",")
+    
+    //generating url
+    let currentWeatherJsonUrl = ("https://api.openweathermap.org/data/2.5/group?id=\(iDsList)&units=metric&APPID=\(apikey)")
+    
+    
+    print("currentWeatherJsonUrl:\(currentWeatherJsonUrl)")
+    
+    
+    guard let currentWeatherUrl = URL(string: currentWeatherJsonUrl) else { return }
+    
+    
+    //Parsing current weather JSON
+    URLSession.shared.dataTask(with: currentWeatherUrl) { (data, response, err) in
+        
+        //check err
+        if err != nil {
+            print("Error:\(String(describing: err))")
+        }
+        
+        
+        guard let data = data else { return }
+       
+        do {
+            
+            let currentWeatherData = try JSONDecoder().decode(WeatherDataJson.self, from: data)
+            
+            weatherList = currentWeatherData.list
+            
+            print(weatherList[3].name)
+            print(weatherList)
+
+            
+        } catch let jsonErr {
+            print("Error:\(jsonErr)")
+        }
+        
+        }.resume()//URLSession
+    
+    
+}//func
 
---
+++ Model/WeatherDataModel.swift
@@ -0,0 +1,126 @@
+//
+//  WeatherDataModel.swift
+//  CurrentWeather
+//
+//  Created by Roman on 2/8/18.
+//  Copyright © 2018 Roman Rudavskiy. All rights reserved.
+//
+
+import Foundation
+
+class WeatherDataJson:Decodable {
+    let cnt:Int
+    let list:[List]
+    
+    init(cnt:Int, list:[List]) {
+        self.cnt = cnt
+        self.list = list
+    }
+}
+
+class List:Decodable {
+    
+    let coord:Coord
+    let sys:Sys
+    let weather:[Weather]
+    let main:Main
+    let visibility:Int?
+    let wind:Wind
+    let clouds:Clouds
+    let dt:Int
+    let id:Int
+    let name:String
+    
+    init(coord:Coord, sys:Sys, weather:[Weather], main:Main, visibility:Int, wind:Wind, clouds:Clouds, dt:Int, id:Int, name:String) {
+        
+        self.coord = coord
+        self.sys = sys
+        self.weather = weather
+        self.main = main
+        self.visibility = visibility
+        self.wind = wind
+        self.clouds = clouds
+        self.dt = dt
+        self.id = id
+        self.name = name
+ }
+    
+}
+
+class Coord:Decodable {
+    let lon:Double
+    let lat:Double
+    
+    init(lon:Double, lat:Double) {
+        self.lon = lon
+        self.lat = lat
+    }
+}
+
+class Sys:Decodable {
+    let type:Int
+    let id:Int
+    let message:Double
+    let country:String
+    let sunrise:Int
+    let sunset:Int
+        
+    init(type:Int, id:Int, message:Double, country:String, sunrise:Int, sunset:Int) {
+        self.type = type
+        self.id = id
+        self.message = message
+        self.country = country
+        self.sunrise = sunrise
+        self.sunset = sunset
+    }
+}
+
+class Weather:Decodable {
+    let id:Int
+    let main:String
+    let description:String
+    let icon:String
+    
+    init(id:Int, main:String, description:String, icon:String) {
+        self.id = id
+        self.main = main
+        self.description = description
+        self.icon = icon
+    }
+}
+
+class Main:Decodable {
+    let temp:Double
+    let pressure:Double
+    let humidity:Int
+    let temp_min:Double
+    let temp_max:Double
+    
+    init(temp:Double, pressure:Double, humidity:Int, temp_min:Double, temp_max:Double) {
+        self.temp = temp
+        self.pressure = pressure
+        self.humidity = humidity
+        self.temp_min = temp_min
+        self.temp_max = temp_max
+    }
+}
+
+class Wind:Decodable {
+    let speed:Double
+    let deg:Double?
+    
+    init(speed:Double, deg:Double) {
+        self.speed = speed
+        self.deg = deg
+    }
+}
+
+class Clouds:Decodable {
+    let all:Int
+    
+    init(all:Int) {
+        self.all = all
+    }
+}
+
+
